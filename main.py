df9b969eaf9a597d564775fe43451bc02927998c
Hideky <soris-orami@live.fr>
2017-07-03 18:41:31 +0200

    Some readjustment for clearer
    + function paintMap() (maybe repaint cause of canvas.delete("all) and reusability)
    Rename main window
    add caps to filename

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import tkinter as tk
 from tkinter import *
 
 def loadMap():
+	global map
+
+	# Open map and parse it as 2D Array
 	with open('./ressources/map.txt') as file:
 		map = [[case for case in line.strip("\n")] for line in file]
 
@@ -12,42 +15,52 @@ def loadMap():
 		for i, case in enumerate(line):
 			if case == '@':
 				line[i] = entities.pop(random.randrange(len(entities)))
-	return map
 
 
-root=Tk()
-root.title("McGyver Escape Game")
-canvas=Canvas(root, width=300, height=300)
+def paintMap():
+	canvas.delete("all")
+	for i, line in enumerate(map):
+		for y, case in enumerate(line):
+			if case == '*':
+				canvas.create_image(y*20+10,i*20+10,image=wall)
+			if case == ' ':
+				canvas.create_image(y*20+10,i*20+10,image=floor)
+			if case == '=':
+				canvas.create_image(y*20+10,i*20+10,image=floor)
+				canvas.create_image(y*20+10,i*20+10,image=rod)
+			if case == '§':
+				canvas.create_image(y*20+10,i*20+10,image=floor)
+				canvas.create_image(y*20+10,i*20+10,image=ether)
+			if case == '|':
+				canvas.create_image(y*20+10,i*20+10,image=floor)
+				canvas.create_image(y*20+10,i*20+10,image=needle)
+			if case == 'M':
+				canvas.create_image(y*20+10,i*20+10,image=floor)
+				canvas.create_image(y*20+10,i*20+10,image=macGyver)
+			if case == 'G':
+				canvas.create_image(y*20+10,i*20+10,image=floor)
+				canvas.create_image(y*20+10,i*20+10,image=gardien)
+
+# Window
+main=Tk()
+main.title("McGyver Escape Game")
+
+# Canvas
+canvas=Canvas(main, width=300, height=300)
 canvas.pack()
 
-map = loadMap()
-floor  = tk.PhotoImage(file="./ressources/floor1.png")
-wall  = tk.PhotoImage(file="./ressources/wall.png")
-needle = tk.PhotoImage(file="./ressources/needle.png")
-rod    = tk.PhotoImage(file="./ressources/rod.png")
-ether  = tk.PhotoImage(file="./ressources/ether.png")
-macGyver    = tk.PhotoImage(file="./ressources/MacGyver.png")
+# Loading Ressources once
+floor    = tk.PhotoImage(file="./ressources/floor1.png")
+wall     = tk.PhotoImage(file="./ressources/wall.png")
+needle   = tk.PhotoImage(file="./ressources/needle.png")
+rod      = tk.PhotoImage(file="./ressources/rod.png")
+ether    = tk.PhotoImage(file="./ressources/ether.png")
+macGyver = tk.PhotoImage(file="./ressources/MacGyver.png")
 gardien  = tk.PhotoImage(file="./ressources/Gardien.png")
-for i, line in enumerate(map):
-	for y, case in enumerate(line):
-		if case == '*':
-			canvas.create_image(y*20+10,i*20+10,image=wall)
-		if case == ' ':
-			canvas.create_image(y*20+10,i*20+10,image=floor)
-		if case == '=':
-			canvas.create_image(y*20+10,i*20+10,image=floor)
-			canvas.create_image(y*20+10,i*20+10,image=rod)
-		if case == '§':
-			canvas.create_image(y*20+10,i*20+10,image=floor)
-			canvas.create_image(y*20+10,i*20+10,image=ether)
-		if case == '|':
-			canvas.create_image(y*20+10,i*20+10,image=floor)
-			canvas.create_image(y*20+10,i*20+10,image=needle)
-		if case == 'M':
-			canvas.create_image(y*20+10,i*20+10,image=floor)
-			canvas.create_image(y*20+10,i*20+10,image=macGyver)
-		if case == 'G':
-			canvas.create_image(y*20+10,i*20+10,image=floor)
-			canvas.create_image(y*20+10,i*20+10,image=gardien)
-			
+map = []
+
+# Loading game
+loadMap()
+paintMap()
+
 mainloop()
