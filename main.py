9dbb285e30be378a69a8aa8aa301ac5b7b6800d5
Hideky <soris-orami@live.fr>
2017-07-05 16:14:47 +0200

    Step 2: Adding movement and victory
    + Light the floor for better visibility

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import random
 import tkinter as tk
 from tkinter import *
+from tkinter import messagebox
 
 def loadMap():
-	global map
+	global map, macGyverPos
 
 	# Open map and parse it as 2D Array
 	with open('./ressources/map.txt') as file:
@@ -11,11 +12,14 @@ def loadMap():
 
 	# Set random position for entities (character/item)
 	entities = ['M', 'G', '|', '=', '§']
-	for line in map:
-		for i, case in enumerate(line):
+	for i, line in enumerate(map):
+		for y, case in enumerate(line):
 			if case == '@':
-				line[i] = entities.pop(random.randrange(len(entities)))
-
+				#entitiesPos.append([entities.pop(random.randrange(len(entities))), i, y])
+				#line[y] = entitiesPos[-1][0]
+				line[y] = entities.pop(random.randrange(len(entities)))
+				if(line[y] == 'M'):
+					macGyverPos = [i, y]
 
 def paintMap():
 	canvas.delete("all")
@@ -41,6 +45,31 @@ def paintMap():
 				canvas.create_image(y*20+10,i*20+10,image=floor)
 				canvas.create_image(y*20+10,i*20+10,image=gardien)
 
+def moveMacGyver(event, sens):
+	global macGyverPos
+	if sens == 'N':
+		newPos = [macGyverPos[0]-1,macGyverPos[1]]
+	if sens == 'E':
+		newPos = [macGyverPos[0],macGyverPos[1]+1]
+	if sens == 'S':
+		newPos = [macGyverPos[0]+1,macGyverPos[1]]
+	if sens == 'W':
+		newPos = [macGyverPos[0],macGyverPos[1]-1]
+
+	if(map[newPos[0]][newPos[1]] == ' '):
+		map[macGyverPos[0]][macGyverPos[1]] = ' '
+		map[newPos[0]][newPos[1]] = 'M'
+		macGyverPos = newPos
+	if(map[newPos[0]][newPos[1]] == 'G'):
+		if(tk.messagebox.askyesno("Game Over", "You Win ! Would you start a new game ?")):
+			loadMap()
+			paintMap()
+		else:
+			main.quit()
+
+	paintMap()
+
+
 # Window
 main=Tk()
 main.title("McGyver Escape Game")
@@ -58,9 +87,14 @@ ether    = tk.PhotoImage(file="./ressources/ether.png")
 macGyver = tk.PhotoImage(file="./ressources/MacGyver.png")
 gardien  = tk.PhotoImage(file="./ressources/Gardien.png")
 map = []
+macGyverPos = []
 
 # Loading game
 loadMap()
 paintMap()
 
-mainloop()
+main.bind("<z>", lambda event, sens='N' : moveMacGyver(event, sens))
+main.bind("<d>", lambda event, sens='E' : moveMacGyver(event, sens))
+main.bind("<s>", lambda event, sens='S' : moveMacGyver(event, sens))
+main.bind("<q>", lambda event, sens='W' : moveMacGyver(event, sens))
+main.mainloop()
